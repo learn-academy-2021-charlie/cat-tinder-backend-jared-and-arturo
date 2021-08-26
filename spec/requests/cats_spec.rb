b274013328db5037a80ff918a9d43f65216448d8
ArtyG-10 <a.gourent@outlook.com>
2021-08-26 15:07:16 -0400

    troubleshoot testing for index and update, implemented testing for create and destroy

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,31 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
+  cat_params = {
+    cat: {
+      name: 'Buster',
+      age: 4,
+      enjoys: 'Meow Mix, and plenty of sunshine'
+    }
+  }
+
   describe "GET /index" do
     it "gets a list of cats" do
-      Cat.create name: "Felix", age: 2, enjoys: "Walks in the park"
-  
+      Cat.create(name: "Felix", age: 2, enjoys: "Walks in the park")
       get '/cats'
       cat = JSON.parse(response.body)
-      expect(response). to have_http_status(200)
-      expect(cat.length). to eq 1
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
     end
   end
 
   describe "POST /create" do
-    it "creates a cat" do
-      cat_params = {
+    it 'creates a cat' do
+      post '/cats', params: cat_params
+      new_cat = Cat.first
+      expect(response). to have_http_status(200)
+      expect(new_cat.name).to eq 'Buster'
+    end
+  end
+
+  describe "PATCH /update" do
+    it "updates a cat" do
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
         cat: {
           name: 'Buster',
-          age: 4, 
-          enjoys: "Meow Mix, and plenty of sunshine",
+          age: 5,
+          enjoys: 'Meow Mix, and plenty of sunshine'
         }
       }
-      post 'cats', params: cat_params
-        expect(response). to have_http_status(200)
-        cat = Cat.first
-        expect(cat.name).to eq 'Buster'
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat = Cat.find(cat.id)
+      expect(response).to have_http_status(200)
+      expect(updated_cat.age).to eq 5
     end
-   
+  end
+
+  describe "DELETE /destroy" do
+    it "deletes a cat" do
+      post '/cats', params: cat_params
+      cat = Cat.first
+      delete "/cats/#{cat.id}"
+      expect(response).to have_http_status(200)
+      cats = Cat.all
+      expect(cats).to be_empty
+    end
   end
 end
